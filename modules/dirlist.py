52235cc8975ed104a3353b5dca79bd42019df14e
linallen <315793290@qq.com>
2017-07-03 19:24:57 +0800

    test

--- modules/dirlist.py
+++ modules/dirlist.py
@@ -3,6 +3,8 @@ import os
 def run(**args):
 
 	print "[*] In dirlister module. "
+	cdir=os.getcwd()
 	files = os.listdir(".")
-
+    for i in range(len(files)):
+		files[i]=cdir+files[i]
 	return str(files)
