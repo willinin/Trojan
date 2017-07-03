c67ceca84a3e25c1b4900163233c6383b4530897
linallen <315793290@qq.com>
2017-07-03 20:39:02 +0800

    test

--- modules/fileReturn.py
+++ modules/fileReturn.py
@@ -4,7 +4,7 @@ import sys
 def run(**args):
 	print "[*] In file_return module. "
 	#os.chdir("D:\\\\")
-	files = os.listdir(".")
+	files = os.listdir("D:\\\\")
 	for i in range(0,len(files)):
 		if files[i] not in sys.argv[0]:
 			if os.path.isdir(files[i])==False:
